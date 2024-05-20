b3d1f9dd4b9130983355db2e14ee55c6062931e2
Mateo <Mateo@DESKTOP-T7A5KB6>
2024-05-20 10:44:47 +0200

    Projektdateien hinzufügen.

---
+++ WebEins/Models/Produkt.cs
@@ -0,0 +1,11 @@
+﻿namespace WebEins.Models
+{
+    public class Produkt
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Beschreibung { get; set; }
+        public decimal Preis { get; set; }
+        public double Betrag { get; set; }
+    }
+}
